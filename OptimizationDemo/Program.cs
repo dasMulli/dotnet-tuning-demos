91d8ea9b410acf29bc26b988c269970c8cf9513a
dasMulli <martin.andreas.ullrich@gmail.com>
2022-10-10 19:46:54 +1100

    Add OptimizationDemo

---
+++ OptimizationDemo/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+
+var stopWatch = new Stopwatch();
+var factory = new Factory();
+
+for (int i = 1; i <= 5; i++)
+{
+    stopWatch.Restart();
+
+    for (int j = 0; j < 10_000; j++)
+    {
+        Workload(factory);
+    }
+
+    stopWatch.Stop();
+
+    Console.WriteLine($"Iteration {i}: {stopWatch.ElapsedMilliseconds} ms");
+}
+
+static void Workload(IFactory factory)
+{
+    var thing = factory.CreateThing();
+    while(!thing.FeelingHappy)
+    {
+        thing.GiveTreat();
+    }
+}
+
+interface IFactory
+{
+    IThing CreateThing();
+}
+
+class Factory : IFactory
+{
+    public IThing CreateThing() => new Thing();
+}
+
+interface IThing
+{
+    bool FeelingHappy { get; }
+    void GiveTreat();
+}
+
+class Thing : IThing
+{
+    private int treatCount = 0;
+    public bool FeelingHappy => treatCount >= 10_000;
+
+    public void GiveTreat()
+    {
+        treatCount++;
+    }
+}
