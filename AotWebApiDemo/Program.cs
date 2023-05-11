9b0a47ab141a5469004864434535cb3bcb290f2e
dasMulli <martin.andreas.ullrich@gmail.com>
2023-05-11 11:07:36 +0200

    Add minimal API AOT demo

---
+++ AotWebApiDemo/Program.cs
@@ -0,0 +1,27 @@
+using AotWebApiDemo;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateSlimBuilder(args);
+builder.Logging.AddConsole();
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.AddContext<TodoJsonContext>();
+});
+
+var app = builder.Build();
+
+var sampleTodos = TodoGenerator.GenerateTodos().ToArray();
+
+var todosApi = app.MapGroup("/todos");
+todosApi.MapGet("/", () => sampleTodos);
+todosApi.MapGet("/{id}", (int id) =>
+    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
+        ? Results.Ok(todo)
+        : Results.NotFound());
+
+app.Run();
+
+[JsonSerializable(typeof(Todo))]
+internal partial class TodoJsonContext : JsonSerializerContext
+{
+}
